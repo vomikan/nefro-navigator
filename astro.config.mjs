2c3a99a3441775a8c280bf38404f1344d56a263b
vomikan <vomikan@mail.ru>
2026-06-05 16:25:31 +0300

    fix: static build for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // 🔑 Имя должно совпадать с названием репозитория на GitHub
   base: '/nefro-navigator/',
   output: 'static',
   integrations: [],
